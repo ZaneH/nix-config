fcd16132a7235cc04b1e25e7ed3a44b343e758a4
Zane Helton <zane99@me.com>
2025-12-07 15:10:58 -0500

    minor sops changes

--- modules/sops.nix
+++ modules/sops.nix
@@ -6,29 +6,31 @@
     age
     ssh-to-age
   ];
-
   sops = {
     validateSopsFiles = true;
-    
     age = {
       keyFile = "/var/lib/sops-nix/key.txt";
     };
     secrets = {
       # SSH private key
-      "id_ed25519" = {
+      "id_rsa_personal" = {
         sopsFile = ./../secrets/ssh.yaml;
-        path = "~/.ssh/id_ed25519";
+        # TODO: Make the uid and path not hard-coded
+        uid = 1000;
+        path = "/home/me/.ssh/id_rsa_personal";
         mode = "0600";
       };
-      "id_ed25519_pub" = {
+      "id_rsa_personal_pub" = {
         sopsFile = ./../secrets/ssh.yaml;
-        path = "~/.ssh/id_ed25519.pub";
+        uid = 1000;
+        path = "/home/me/.ssh/id_rsa_personal.pub";
         mode = "0600";
       };
       # Git configuration
       "gitconfig" = {
         sopsFile = ./../secrets/git.yaml;
-        path = "~/.gitconfig";
+        uid = 1000;
+        path = "/home/me/.gitconfig";
         mode = "0644";
       };
     };
