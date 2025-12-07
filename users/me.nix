373ea34250cb4c153ec210a1c402df8c9b77e31a
Zane Helton <zane99@me.com>
2025-12-07 16:05:47 -0500

    set default shell

--- users/me.nix
+++ users/me.nix
@@ -23,20 +23,19 @@ in
     initialHashedPassword = "$y$j9T$HY94rD7gaT78XgT3mZVlW/$C6rSif/qBW5A.l/UFTrnv009o5U0z5dvK8oDYRSSKH4"; # "password"
   };
 
+  programs.zsh.enable = true;
+  users.users.${username}.shell = pkgs.zsh;
+
   # 2) Wire up Home-Manager
   home-manager.users = {
     "${username}" = {
       home.username = username;
       home.homeDirectory = "/home/${username}";
-
       imports = [
         ../modules/kde-home.nix
         ../modules/development.nix
         ../modules/entertainment.nix
       ];
-
-      programs.zsh.enable = true;
-
       # 3) Inject your per-user package list here
       home.packages = userPackages;
     };
