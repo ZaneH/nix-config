0e43d5e34263f7f45432aa2cf041f8daa0affcaa
Zane Helton <zane99@me.com>
2025-12-07 22:38:51 -0500

    update some aliases

--- dotfiles/alias.zsh
+++ dotfiles/alias.zsh
@@ -22,3 +22,24 @@ alias n="$EDITOR ."
 alias pbcopy="xclip -selection clipboard"
 alias yt3="yt-dlp -f bestaudio -x --audio-format mp3 --audio-quality 0 --downloader aria2c --downloader-args '-c -j 3 -x 3 -s 3 -k 1M'"
 alias e="emacs -nw"
+
+# Fetch gitignore
+dl-gitignore() {
+  if [[ -z "$1" ]]; then
+    echo "usage: dl-gitignore <language_name>"
+    return 1
+  fi
+
+  curl -fsSL https://raw.githubusercontent.com/github/gitignore/main/${1}.gitignore
+}
+
+# Fetch license
+dl-license() {
+  if [[ -z "$1" ]]; then
+    echo "usage: dl-license <license_name>"
+    return 1
+  fi
+
+  local name="${1:l}"
+  curl -fsSL "https://raw.githubusercontent.com/licenses/license-templates/master/templates/${name}.txt"
+}
