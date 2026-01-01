22de47b68d627714f6cc53c295b160f711703974
Zane Helton <zane99@me.com>
2026-01-01 01:13:53 -0500

    Add gobin path

--- dotfiles/local.zsh
+++ dotfiles/local.zsh
@@ -1,2 +1,5 @@
 # Put things here that are very specific to your machine
 # This is a great place to put cargo or npm init commands
+
+export PATH=$HOME/.npm-global/bin:$PATH
+export PATH=$HOME/go/bin:$PATH
