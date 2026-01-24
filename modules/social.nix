072fb0c4146ba9fe89495cc8554ebd4c813bf907
Zane Helton <zane99@me.com>
2026-01-24 16:31:33 -0500

    Add Slacky for ARM

--- modules/social.nix
+++ modules/social.nix
@@ -1,6 +1,7 @@
 { pkgs, lib, ... }:
 let
   isX86Linux = pkgs.stdenv.hostPlatform.system == "x86_64-linux";
+  isARMLinux = pkgs.stdenv.hostPlatform.system == "aarch64-linux";
 in
 {
   home.packages = with pkgs; [
@@ -9,5 +10,7 @@ in
     slack
     discord
     discord-canary
+  ] ++ lib.optionals isARMLinux [
+    slacky
   ];
 }
