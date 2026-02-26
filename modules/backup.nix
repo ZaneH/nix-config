bf34ef880881be532510f0bf60bd9b90314dc7ae
Zane Helton <zane99@me.com>
2026-02-26 15:38:15 -0500

    Put backups on a schedule

--- modules/backup.nix
+++ modules/backup.nix
@@ -1,6 +1,50 @@
 { pkgs, ... }:
 {
-  services.borgmatic = {
-    enable = true;
+  environment.systemPackages = with pkgs; [
+    borgmatic
+  ];
+
+  systemd.services.borgmatic-repos = {
+    description = "Run borgmatic repos backup";
+    serviceConfig = {
+      Type = "oneshot";
+      User = "me";
+      Group = "users";
+      ExecStart = "${pkgs.borgmatic}/bin/borgmatic --config /home/me/.config/borgmatic/config.yaml";
+    };
+  };
+
+  systemd.timers.borgmatic-repos = {
+    description = "Run borgmatic for repos every other day";
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnCalendar = "*-*-1/2 03:00:00";
+      Persistent = true;
+      RandomizedDelaySec = "30m";
+      AccuracySec = "1h";
+      Unit = "borgmatic-repos.service";
+    };
+  };
+
+  systemd.services.borgmatic-nas = {
+    description = "Run borgmatic NAS backup";
+    serviceConfig = {
+      Type = "oneshot";
+      User = "me";
+      Group = "users";
+      ExecStart = "${pkgs.borgmatic}/bin/borgmatic --config /home/me/.config/borgmatic/config-truenas.yaml";
+    };
+  };
+
+  systemd.timers.borgmatic-nas = {
+    description = "Run borgmatic NAS twice a week";
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnCalendar = "Mon,Thu 03:00:00";
+      Persistent = true;
+      RandomizedDelaySec = "30m";
+      AccuracySec = "1h";
+      Unit = "borgmatic-nas.service";
+    };
   };
 }
